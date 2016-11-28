8c0f2526d1ec11753f7613a640830cd27b7c93a6
jakobvk <jakob.von.kietzell@fh-potsdam.de>
2016-11-28 14:56:47 +0100

    background in setup

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function setup () {
 
 createCanvas (500,500)
-
+background (100,200,10);
 
 }
 
 function draw () {
-background (100,200,10);
+
 fill (20,30,10);
 ellipse (width/2,height/2,10,20);
 
